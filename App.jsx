84ebba596527272ebe05c77795cc74127a9a572f
Anjali <111136928+anjali077@users.noreply.github.com>
2024-12-17 14:54:59 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+ 
+import Navbar from './components/Navbar'; 
+import Herosection from './components/Herosection'; 
+import Course from './components/Course'; 
+import LoginPage from './Client/LoginPage'; 
+import Signup from './Client/Signup'; 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css'; 
+
+const HomePage = () => {
+  return (
+    <div>
+      <Herosection />
+      <Course />
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* Route for home displaying Herosection and Course */}
+        <Route path="/" element={<HomePage />} />
+        {/* Route for signup */}
+        <Route path="/signup" element={<Signup />} />
+        {/* Route for login */}
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
